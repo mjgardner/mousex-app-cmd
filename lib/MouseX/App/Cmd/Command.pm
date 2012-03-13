aaf69a7287e03a6461944173419629be577025b8
Mark Gardner <gardnerm@gsicommerce.com>
2012-03-13 13:25:43 -0400

    Build results of d97fd72 (on master)

--- lib/MouseX/App/Cmd/Command.pm
+++ lib/MouseX/App/Cmd/Command.pm
@@ -3,7 +3,7 @@ use 5.006;
 use strict;
 use warnings;
 
-# VERSION
+our $VERSION = '0.09';    # VERSION
 use Mouse;
 use English '-no_match_vars';
 use Getopt::Long::Descriptive ();
@@ -73,6 +73,22 @@ sub _usage_format {    ## no critic (ProhibitUnusedPrivateSubroutines)
 
 # ABSTRACT: Base class for commands.
 
+__END__
+
+=pod
+
+=for :stopwords Yuval Kogman Guillermo Roditi Mark Gardner Infinity Interactive cpan
+testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto
+metadata placeholders metacpan
+
+=head1 NAME
+
+MouseX::App::Cmd::Command - Base class for commands.
+
+=head1 VERSION
+
+version 0.09
+
 =head1 SYNOPSIS
 
     use Mouse;
@@ -99,7 +115,9 @@ This is a replacement base class for L<App::Cmd::Command|App::Cmd::Command>
 classes that includes
 L<MouseX::Getopt|MouseX::Getopt> and the glue to combine the two.
 
-=method _process_args
+=head1 METHODS
+
+=head2 _process_args
 
 Replaces L<App::Cmd::Command|App::Cmd::Command>'s argument processing in in
 favor of
@@ -114,3 +132,119 @@ L<MouseX::Getopt|MouseX::Getopt> based processing.
 =item L<MouseX::Getopt|MouseX::Getopt>
 
 =back
+
+=head1 SUPPORT
+
+=head2 Perldoc
+
+You can find documentation for this module with the perldoc command.
+
+  perldoc MouseX::App::Cmd
+
+=head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
+
+=over 4
+
+=item *
+
+Search CPAN
+
+The default CPAN search engine, useful to view POD in HTML format.
+
+L<http://search.cpan.org/dist/MouseX-App-Cmd>
+
+=item *
+
+AnnoCPAN
+
+The AnnoCPAN is a website that allows community annotations of Perl module documentation.
+
+L<http://annocpan.org/dist/MouseX-App-Cmd>
+
+=item *
+
+CPAN Ratings
+
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
+L<http://cpanratings.perl.org/d/MouseX-App-Cmd>
+
+=item *
+
+CPANTS
+
+The CPANTS is a website that analyzes the Kwalitee ( code metrics ) of a distribution.
+
+L<http://cpants.perl.org/dist/overview/MouseX-App-Cmd>
+
+=item *
+
+CPAN Testers
+
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
+
+L<http://www.cpantesters.org/distro/M/MouseX-App-Cmd>
+
+=item *
+
+CPAN Testers Matrix
+
+The CPAN Testers Matrix is a website that provides a visual overview of the test results for a distribution on various Perls/platforms.
+
+L<http://matrix.cpantesters.org/?dist=MouseX-App-Cmd>
+
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=MouseX::App::Cmd>
+
+=back
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through the web
+interface at L<https://github.com/mjgardner/mousex-app-cmd/issues>. You will be automatically notified of any
+progress on the request by the system.
+
+=head2 Source Code
+
+The code is open to the world, and available for you to hack on. Please feel free to browse it and play
+with it, or whatever. If you want to contribute patches, please send me a diff or prod me to pull
+from your repository :)
+
+L<https://github.com/mjgardner/mousex-app-cmd>
+
+  git clone git://github.com/mjgardner/mousex-app-cmd.git
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Yuval Kogman <nothingmuch@woobling.org>
+
+=item *
+
+Guillermo Roditi <groditi@cpan.org>
+
+=item *
+
+Mark Gardner <mjgardner@cpan.org>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Infinity Interactive.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
